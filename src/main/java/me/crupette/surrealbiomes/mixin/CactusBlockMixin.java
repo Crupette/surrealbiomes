ec6d966d891f108e2a775305c41091086f6f674b
Jon Santmyer <jonsantmyer@gmail.com>
2020-05-24 17:47:34 -0400

    Removed override mixins, replaced with INJECT added mixin for rabbit spawning

--- src/main/java/me/crupette/surrealbiomes/mixin/CactusBlockMixin.java
+++ src/main/java/me/crupette/surrealbiomes/mixin/CactusBlockMixin.java
@@ -9,34 +9,18 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Iterator;
 
 @Mixin(CactusBlock.class)
-public abstract class CactusBlockMixin extends Block {
+public abstract class CactusBlockMixin{
 
-    public CactusBlockMixin(Settings settings) {
-        super(settings);
-    }
-
-    //I'm bad at mixins.
-    @Override
-    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
-        Iterator var4 = Direction.Type.HORIZONTAL.iterator();
-
-        Direction direction;
-        Material material;
-        do {
-            if (!var4.hasNext()) {
-                BlockState blockState2 = world.getBlockState(pos.down());
-                return (blockState2.isOf(Blocks.CACTUS) || blockState2.getBlock() instanceof SandBlock) && !world.getBlockState(pos.up()).getMaterial().isLiquid();
-            }
-
-            direction = (Direction)var4.next();
-            BlockState blockState = world.getBlockState(pos.offset(direction));
-            material = blockState.getMaterial();
-        } while(!material.isSolid() && !world.getFluidState(pos.offset(direction)).matches(FluidTags.LAVA));
-
-        return false;
+    @Inject(method = "canPlaceAt",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/WorldView;getBlockState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/BlockState;", ordinal = 1),
+            cancellable = true)
+    public void colorPlacePatch(BlockState state, WorldView world, BlockPos pos, CallbackInfoReturnable<Boolean> ci) {
+        BlockState low = world.getBlockState(pos.down());
+        if(low.getBlock() instanceof SandBlock && !world.getBlockState(pos.up()).getMaterial().isLiquid()) ci.setReturnValue(true);
     }
 }
--- src/main/java/me/crupette/surrealbiomes/mixin/DeadBushBlockMixin.java
+++ src/main/java/me/crupette/surrealbiomes/mixin/DeadBushBlockMixin.java
@@ -5,17 +5,18 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(DeadBushBlock.class)
-public abstract class DeadBushBlockMixin extends PlantBlock {
-    protected DeadBushBlockMixin(Settings settings) {
-        super(settings);
-    }
+public abstract class DeadBushBlockMixin{
 
-    @Override
-    public boolean canPlantOnTop(BlockState floor, BlockView world, BlockPos pos) {
+    @Inject(method="canPlantOnTop",
+    at = @At(value="HEAD"), cancellable = true)
+    public void colorsandpatch(BlockState floor, BlockView world, BlockPos pos, CallbackInfoReturnable<Boolean> ci) {
         Block block = floor.getBlock();
-        return block instanceof SandBlock || block == Blocks.TERRACOTTA || block == Blocks.WHITE_TERRACOTTA || block == Blocks.ORANGE_TERRACOTTA || block == Blocks.MAGENTA_TERRACOTTA || block == Blocks.LIGHT_BLUE_TERRACOTTA || block == Blocks.YELLOW_TERRACOTTA || block == Blocks.LIME_TERRACOTTA || block == Blocks.PINK_TERRACOTTA || block == Blocks.GRAY_TERRACOTTA || block == Blocks.LIGHT_GRAY_TERRACOTTA || block == Blocks.CYAN_TERRACOTTA || block == Blocks.PURPLE_TERRACOTTA || block == Blocks.BLUE_TERRACOTTA || block == Blocks.BROWN_TERRACOTTA || block == Blocks.GREEN_TERRACOTTA || block == Blocks.RED_TERRACOTTA || block == Blocks.BLACK_TERRACOTTA || block == Blocks.DIRT || block == Blocks.COARSE_DIRT || block == Blocks.PODZOL;
+        if(block instanceof SandBlock) ci.setReturnValue(true);
     }
 }
