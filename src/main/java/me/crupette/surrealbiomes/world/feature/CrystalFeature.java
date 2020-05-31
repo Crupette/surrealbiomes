61e88dc30a50579d369181fe7a00c2a6bcd2e472
Jon Santmyer <jonsantmyer@gmail.com>
2020-05-31 16:36:18 -0400

    crystal feature refactor build process upgrade

--- src/main/java/me/crupette/surrealbiomes/world/feature/CrystalFeature.java
+++ src/main/java/me/crupette/surrealbiomes/world/feature/CrystalFeature.java
@@ -3,7 +3,9 @@ package me.crupette.surrealbiomes.world.feature;
 import com.mojang.datafixers.Dynamic;
 import me.crupette.surrealbiomes.block.SurrealBlocks;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.chunk.ChunkGeneratorConfig;
@@ -21,39 +23,33 @@ public class CrystalFeature extends Feature<CrystalFeatureConfig> {
     private void generateCrystal(IWorld world, Random random, BlockPos center, CrystalFeatureConfig config){
         int height = random.nextInt(config.maxHeight - config.minHeight) + config.minHeight;
         int radius = random.nextInt(config.maxRadius - config.minRadius) + config.minRadius;
-        float slant = random.nextFloat() * config.tilt;
-        float dir = random.nextFloat() * 360.f;
+        float roll = random.nextFloat() * 360.f;
+        float pitch = (random.nextFloat() * 5.f) + 87.5f;
+        float tilt = random.nextFloat() * config.tilt;
         int spread = config.spread;
-        BlockPos pos = new BlockPos(center.getX() + (random.nextInt(spread) - spread),
-                                    center.getY(),
-                                    center.getZ() + (random.nextInt(spread) - spread));
+        BlockPos pos = new BlockPos(center.getX() + (random.nextInt(spread * 2) - spread),
+                center.getY(),
+                center.getZ() + (random.nextInt(spread * 2) - spread));
 
         while (world.isAir(pos) && pos.getY() > 2) pos = pos.down();
-        if(world.getBlockState(pos).getBlock() != SurrealBlocks.CRYSTAL_GRASS) return;
+        if(!world.getBlockState(pos).getBlock().equals(SurrealBlocks.CRYSTAL_GRASS)) return;
         pos = pos.up();
 
-        double addx = 0, addz = 0;
+        Vec3d rot = new Vec3d(Math.toRadians(roll), Math.toRadians(pitch),0);
+        Vec3d forward = new Vec3d(Math.cos(rot.y) * Math.sin(rot.x),
+                Math.sin(rot.y),
+                Math.sin(rot.y) * Math.cos(rot.x));
+
         int color = random.nextInt(config.composition.size());
         BlockState block = config.composition.get(color);
-        for(int y = 0; y <= height; y++){
-            if(y >= height - 4) radius--;
-            for(int x = -radius; x <= radius; x++) {
-                for(int z = -radius; z <= radius; z++) {
-                    int distcheck = Math.abs(x) + Math.abs(z);
-                    if(distcheck > radius) continue;
-                    if(distcheck == radius || y == height){
-                        if(world.isAir(pos.add(x, 0, z))) world.setBlockState(pos.add(x, 0, z), block, 2);
-                    }else {
-                        world.setBlockState(pos.add(x, 0, z), block, 2);
-                    }
-                }
-            }
-            addx += Math.sin(dir) * slant;
-            addz += Math.sin(dir) * slant;
 
-            pos = pos.add(addx > 1 ? 1 : 0, 1, addz > 1 ? 1 : 0);
-            addx = addx > 1 ? 0 : addx;
-            addz = addz > 1 ? 0 : addz;
+        for(int len = 0; len < height; len++){
+            BlockPos placePos = new BlockPos(forward.x * len * tilt, len, forward.z * len * tilt);//new BlockPos(forward.x * len, forward.y * len, forward.z * len);
+            for(int x = -radius; x <= radius; x++) for(int y = -radius; y <= radius; y++) for(int z = -radius; z <= radius; z++){
+                if(Math.abs(x) + Math.abs(y) + Math.abs(z) > radius) continue;
+                if(!world.getBlockState(placePos.add(x, y, z).add(pos)).getBlock().equals(Blocks.AIR)) continue;
+                world.setBlockState(placePos.add(x, y, z).add(pos), block, 2);
+            }
         }
     }
 
